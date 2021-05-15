76a412dd8abcc76331152a5adfbb10102d0365a0
oguzdurmaz-dev <oz@ozzdigitalworks.com>
2021-05-15 10:20:22 +0300

    Pages and routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is an app to find lastest concerts and musiccal events.</p>
+      <p>Version 1.0.0</p>
+    </div>
+  );
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function EventPage() {
+    return (
+        <div>
+            <h1>My event page</h1>
+        </div>
+    )
+}
