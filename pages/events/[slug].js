d45dadd85f0e6ac0205b74d76945d6972518b6da
oguzdurmaz-dev <oz@ozzdigitalworks.com>
2021-05-15 12:28:35 +0300

    jsconfig

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,11 +1,14 @@
-import Layout from "../../components/Layout"
+import Layout from "@/components/Layout"
+import { useRouter } from 'next/router'
 
 export default function EventPage() {
     const router = useRouter();
     console.log(router)
+
     return (
         <Layout>
             <h1>My event page</h1>
+         
         </Layout>
     )
 }
