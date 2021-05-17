7b47bbb9940acca3a3fb6d1f86ff6a1a6799fef2
oguzdurmaz-dev <oz@ozzdigitalworks.com>
2021-05-17 14:24:02 +0300

    nasd

--- components/Header.js
+++ components/Header.js
@@ -1,37 +1,71 @@
-import Link from "next/link"
-import styles from "../styles/Header.module.css"
-import Search from "./Search"
+import Link from "next/link";
+import { useContext } from "react";
+import AuthContext from "@/context/AuthContext";
+import styles from "../styles/Header.module.css";
+import Search from "./Search";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 export default function Header() {
-    return (
-        <header className={styles.header}>
-            <div className={styles.logo}>
-                <Link href="/">
-                    <a>Concert Events</a>
+  const { user, logout } = useContext(AuthContext);
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href="/">
+          <a>Concert Events</a>
+        </Link>
+      </div>
+      <div>
+        <Search />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/events">
+              <a>Events</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          {user ? (
+            //if logged in
+            <>
+              {" "}
+              <li>
+                <Link href="/events/add">
+                  <a>Add Event</a>
                 </Link>
-            </div>
-            <div>
-                <Search/>
-            </div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/events">
-                            <a>Events</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/events/add">
-                            <a>Add Event</a>
-                        </Link>
-                    </li>
-                    
-                    <li>
-                        <Link href="/about">
-                            <a>About</a>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+              </li>
+              <li>
+                <Link href="account/dashboard">
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  className="btn-secondary btn-icon"
+                  onClick={() => logout()}
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            //if logged out
+            <>
+              {" "}
+              <li>
+                <Link href="/account/login">
+                  <a className="btn-secondary btn-icon">
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
 }
