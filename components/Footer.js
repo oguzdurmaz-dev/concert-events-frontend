03d115112bb9cb33f4d5e7a1f9684dc2409bcf63
Oğuz <53166823+oguzdurmaz-dev@users.noreply.github.com>
2021-05-23 17:29:32 +0300

    Add files via upload

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,11 @@
-export default function FooterPage() {
-  return (
-    <div
-      className="flex justify-center items-center p-1 bg-gray-800 text-gray-200 mt-10 w-full absolute "
-      style={{ 'bottom': "0" }}
-    >
-      <h2 style={{ "fontSize": "13px" }}>
-        Chatlify 1.0 - Built by Oguz Durmaz
-      </h2>
-    </div>
-  );
+
+import Link from "next/link"
+import styles from "../styles/Footer.module.css"
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p>Copyright &copy; Concert Events 2021</p>
+            <Link href="/about">About this page</Link>
+        </footer>
+    )
 }
